7d99e9a43f5cabb927e5ec7109793442a72f4922
Nicolas Sicolo <sicolo2208@gmail.com>
2024-04-13 15:50:17 -0300

    Cambios varios

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,24 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+
+--Ejemplo Aplicacion Parcial de una Funcion
+
+esMayorDeEdad :: Number -> Bool
+esMayorDeEdad edad = edad >= 18
+
+type Persona = (String, Number)
+
+edadPersona :: Persona -> Number
+--edadPersona persona = snd persona
+edadPersona = snd
+
+--Saber si una persona es mayor de edad
+
+personaMayorDeEdad :: Persona -> Bool
+--personaMayorDeEdad persona = (esMayorDeEdad . edadPersona) persona
+personaMayorDeEdad = esMayorDeEdad . edadPersona
+
+
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -8,4 +8,15 @@ correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
+  describe "Test de funcion esMayorDeEdad" $ do
+    it "Una persona con 19 años es mayor de edad" $ do
+      esMayorDeEdad 19 `shouldBe` True
+    it "Una persona con 8 años no es mayor de edad" $ do
+      esMayorDeEdad 8 `shouldBe` False
+  describe "Test de funcion edadPersona" $ do
+    it "La edad de ramiro es de 19 años" $ do
+      edadPersona ("Ramiro", 19) `shouldBe` 19
+  describe "Test de funcion personaMayorDeEdad" $ do
+    it "Ramiro es mayor de edad" $ do
+      personaMayorDeEdad ("Ramiro", 19) `shouldBe` True
 
